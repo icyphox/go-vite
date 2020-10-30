c0b82b5ec6f99973d963088f56ca1b237d1743ac
Anirudh Oppiliappan <x@icyphox.sh>
2020-10-30 11:55:40 +0530

    Syntax highlighting!

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/icyphox/go-vite
 go 1.13
 
 require (
+	github.com/Depado/bfchroma v1.3.0
 	github.com/adrg/frontmatter v0.1.0
+	github.com/alecthomas/chroma v0.7.3
 	github.com/cross-cpm/go-shutil v0.0.0-20190908093542-3fcbb1a2151e
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1
--- markdown.go
+++ markdown.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	bf "github.com/russross/blackfriday/v2"
+    bfc "github.com/Depado/bfchroma"
 )
 
 var bfFlags = bf.UseXHTML | bf.Smartypants | bf.SmartypantsFractions |
-	bf.SmartypantsDashes | bf.SmartypantsAngledQuotes | bf.NofollowLinks |
-	bf.FootnoteReturnLinks
+	bf.SmartypantsDashes | bf.NofollowLinks | bf.FootnoteReturnLinks
 
 func mdRender(input []byte) []byte {
 	return bf.Run(
-		input,
-		bf.WithRenderer(bf.NewHTMLRenderer(bf.HTMLRendererParameters{
-			Flags: bfFlags,
-		})),
+        input,
+        bf.WithRenderer(
+            bfc.NewRenderer(
+                bfc.Style("bw"),
+                bfc.Extend(
+                    bf.NewHTMLRenderer(bf.HTMLRendererParameters{
+                        Flags: bfFlags,
+                    }),
+                ),
+            ),
+        ),
 	)
 }
