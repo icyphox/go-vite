bc18e19877fdb3475feb5456afaadf29dd873df7
Anirudh Oppiliappan <x@icyphox.sh>
2023-03-19 12:20:58 +0200

    nix: setup gomod2nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
-  description = "vite";
+  description = "a fast and minimal static site generator";
 
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go
-          gotools
-          gnumake
-        ];
-      };
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
+
+  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ gomod2nix.overlays.default ];
+          };
+
+        in
+        {
+          packages.default = pkgs.callPackage ./. { };
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        })
+    );
 }
